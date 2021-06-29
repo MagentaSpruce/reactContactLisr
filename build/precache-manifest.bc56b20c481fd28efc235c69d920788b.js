9051eea837fac7f401393320a1278786a0b36358
MagentaSpruce <81999424+MagentaSpruce@users.noreply.github.com>
2021-06-28 22:03:01 -0400

    Set up for React deployment

---
+++ build/precache-manifest.bc56b20c481fd28efc235c69d920788b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688aa30dc6879b587af7af7077a02f7a",
+    "url": "/reactContactList/index.html"
+  },
+  {
+    "revision": "a48f9d490ac6c260b2fd",
+    "url": "/reactContactList/static/js/2.41d2c938.chunk.js"
+  },
+  {
+    "revision": "28b2e33a5742c43c91f7",
+    "url": "/reactContactList/static/js/main.fe0b9e13.chunk.js"
+  },
+  {
+    "revision": "0f0f46ded2b068686261",
+    "url": "/reactContactList/static/js/runtime-main.b7ae98d5.js"
+  }
+]);
